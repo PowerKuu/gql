9cbf10d0a497f916531317c9fc9f4708403439f3
PowerKuu <hkleven6@outlook.com>
2022-11-28 19:23:39 +0100

    update

--- gql-client.ts
+++ gql-client.ts
@@ -1,4 +1,9 @@
 import fetch, {HeadersInit} from "node-fetch"
+import { readFileSync } from "fs"
+import { resolve } from "path"
+
+import gql from 'graphql-tag';
+import { print } from 'graphql'
 
 export interface Connection {
     url: string,
@@ -9,10 +14,40 @@ export interface GQL {
     [name:string]: string
 }
 
-export default class GqlClient<T extends GQL> {
-    constructor(public connection:Connection, public GQL:T) {}
 
-    async run(name:keyof T, variables:{[key: string]: any} = {}) {
+export function parseGraphqlObject(path:string): GQL {
+    const rawGQL = readFileSync(resolve(process.cwd(), path), {
+        encoding: "utf-8"
+    })
+
+    const GQL = {}
+    const parsed = gql(rawGQL)
+
+    for (var definition of parsed.definitions) {
+        if (definition.kind !== "OperationDefinition") continue
+
+        const operation = definition["operation"]
+        const nameObject = definition["name"]
+        if ((operation === "query" || operation === "mutation") && nameObject) {
+            const nameError = () => {throw new Error(`Graphql ${operation} must have a name!`)}
+            if (!nameObject || !nameObject["value"]) nameError()
+            const name = nameObject["value"]
+    
+            GQL[name] = print(definition)
+        }
+    }
+
+    return GQL
+}
+
+
+export default class GqlClient {
+    GQL:GQL
+    constructor(public connection:Connection, public graphqlPath:string) {
+        this.GQL = parseGraphqlObject(this.graphqlPath)
+    }
+
+    async run(name:string, variables:{[key: string]: any} = {}) {
         const data = await fetch(this.connection.url, {
             method: 'POST',
             headers: {
